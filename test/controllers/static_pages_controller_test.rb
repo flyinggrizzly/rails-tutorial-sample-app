7796ff60371942bccad3436746fc61be9b0966d2
SeanDMR <sean@flyinggrizzly.io>
2017-03-28 19:26:46 +0100

    add helper and integration tests for layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,30 +7,30 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'should get root' do
-    get root_url
+    get root_path
     assert_response :success
     assert_select('title', @basetitle)
   end
 
   test 'should get home' do
-    get home_url
+    get home_path
     assert_response :redirect
   end
 
   test 'should get help' do
-    get help_url
+    get help_path
     assert_response :success
     assert_select('title', "Help | #{@basetitle}")
   end
 
   test 'should get about' do
-    get about_url
+    get about_path
     assert_response :success
     assert_select('title', "About | #{@basetitle}")
   end
 
   test 'should get contact' do
-    get contact_url
+    get contact_path
     assert_response :success
     assert_select('title', "Contact | #{@basetitle}")
   end
