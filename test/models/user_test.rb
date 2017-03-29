4dca2d19a506cf5ead702f76f6552f96f1b2615e
SeanDMR <sean@flyinggrizzly.io>
2017-03-29 07:14:08 +0100

    add user model validation tests, validations

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,19 @@ class UserTest < ActiveSupport::TestCase
     @user = User.new(name: 'Example User', email: 'user@example.com')
   end
 
+  # baseline: a new user should be valid
   test 'should be valid' do
     assert @user.valid?
+  end
+
+  # check that we are insisting that users have names
+  test 'name should be present' do
+    @user.name = '  '
+    assert_not @user.valid?
+  end
+
+  test 'email should be present' do
+    @user.email = '  '
+    assert_not @user.valid?
   end
 end
