d31043c9e434adf2d5c440442581c4b9d2b3792d
Sean DMR <sean@flyinggrizzly.io>
2017-04-08 14:07:08 +0100

    fresh start

--- app/controllers/application_controller.rb
+++
@@ -1,7 +0,0 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-
-  def flying_grizzly
-    render html: "flap, flap, grr"
-  end
-end
--- test/controllers/users_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
-    assert_select('title', full_title('Sign up'))
-  end
-end
