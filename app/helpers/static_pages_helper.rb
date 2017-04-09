6c9780a39b0a625a72c92b1c35888f38a40ad89d
SeanDMR <sean@flyinggrizzly.io>
2017-04-09 20:08:34 +0100

    add title helper, abstract layout to app.html.erb

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,12 @@
 module StaticPagesHelper
+
+  def full_title(title = '')
+    base_title = 'Rails Tutorial Sample App'
+    if title.empty?
+      full_title = base_title
+    else
+      full_title = title + ' | ' + base_title
+    end
+    full_title
+  end
 end
---
+++ test/helpers/static_pages_helper_test.rb
@@ -0,0 +1,16 @@
+# require 'test_helper'
+#
+# class StaticPagesHelperTest
+#   def setup
+#     @base_title = 'Rails Tutorial Sample App'
+#   end
+#
+#   test 'empty title should produce base title' do
+#     assert_equal(full_title, @base_title)
+#   end
+#
+#   test 'title string should produce combined title' do
+#     assert_equal(full_title('Hello'), "Hello | #{@base_title}")
+#   end
+#
+# end
