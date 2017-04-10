8503c7b48ad2179b1ced6b80bda2fe791e42732a
SeanDMR <sean@flyinggrizzly.io>
2017-04-10 13:33:13 +0100

    prettify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
   root 'static_pages#home'
-  get  'static_pages/home', to: redirect('/')
-  get  'static_pages/help'
-  get  'static_pages/about'
-  get  'static_pages/contact'
+  get  '/home',    to: redirect('/')
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
 
 end
