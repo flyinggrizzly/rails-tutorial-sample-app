e9615ede7087272c32b948ea46f5f501b98d14a9
SeanDMR <sean@flyinggrizzly.io>
2017-04-18 09:46:12 -0700

    add sessions controller, test

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
 
+  # statics
   root 'static_pages#home'
   get  '/home',    to: redirect('/')
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
 
-
-
+  # user creation and display
   resources :users
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+
+  # login/logout
+  get    '/login',  to: 'sessions#new'
+  post   'login',   to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
