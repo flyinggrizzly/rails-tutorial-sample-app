fe909fb2580b695dbed0b8fb3fa4745f97d18018
SeanDMR <sean@flyinggrizzly.io>
2017-04-23 17:54:22 +0100

    integration test for login form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Log in the user and redirect to the user's show page
+    else
+      flash[:danger] = 'Invalid email/password combination' # not quite right
+      render 'new'
+    end
   end
 
   def destroy
