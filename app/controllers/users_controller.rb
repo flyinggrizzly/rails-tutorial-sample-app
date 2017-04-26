c971c34914688a9e5747aeb8026d2c88bee45257
SeanDMR <sean@flyinggrizzly.io>
2017-04-26 07:55:45 +0100

    protect updated and edit for only the owning user to access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def show
     @user = User.find(params[:id])
@@ -21,11 +22,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # Don't need to look up the user to edit; it's handled by before_action :correct_user
   end
 
   def update
-    @user = User.find(params[:id])
+    # Don't need to look up the user to edit; it's handled by before_action :correct_user
     if @user.update_attributes(user_params)
       flash[:success] = 'Profile updated'
       redirect_to @user
@@ -47,5 +48,10 @@ class UsersController < ApplicationController
       flash[:danger] = 'Please log in.'
       redirect_to login_url
     end
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless current_user?(@user)
   end
 end
