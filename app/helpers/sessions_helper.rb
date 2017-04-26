52a2d51844e33c5cc33093c829e747817bf38532
SeanDMR <sean@flyinggrizzly.io>
2017-04-26 07:55:45 +0100

    protect updated and edit for only the owning user to access

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -46,4 +46,9 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  # Returns true if given user is the current user
+  def current_user?(user)
+    user == current_user
+  end
 end
