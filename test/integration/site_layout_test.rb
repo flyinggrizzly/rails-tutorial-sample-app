7df717c1e0f5a8a5cb4e6c9bf9c78eaee66f317d
SeanDMR <sean@flyinggrizzly.io>
2017-04-26 21:42:23 +0100

    add index action for users

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
 
-  test 'layout links' do
+  test 'logged out layout links' do
     get root_path
     assert_template 'static_pages/home'
     assert_select 'a[href=?]', root_path, count: 2
@@ -11,6 +11,21 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
     assert_select 'a[href=?]', login_path
+  end
+
+  test 'logged in layout links' do
+    user = users(:didi)
+    log_in_as user
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
+    assert_select 'a[href=?]', signup_path
+    assert_select 'a[href=?]', edit_user_path(user)
+    assert_select 'a[href=?]', user_path(user)
+    assert_select 'a[href=?]', logout_path
   end
 
   test 'pages have correct titles' do
