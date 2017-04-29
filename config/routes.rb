2f0cdefff923842377419883ba9d65dfbb58df94
SeanDMR <sean@flyinggrizzly.io>
2017-04-29 20:10:14 +0100

    add account activation resource, restrict routes to edit

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   resources :users
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  resources :account_activations, only: [:edit]
 
   # login/logout
   get    '/login',  to: 'sessions#new'
