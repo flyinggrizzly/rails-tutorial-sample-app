2a9f84bc6a85b1e1289f71f4c611fef370734dd0
SeanDMR <sean@flyinggrizzly.io>
2017-05-05 20:37:44 +0100

    fix carrier wave gcp config

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,9 +3,9 @@ if Rails.env.production?
     conf.fog_credentials = {
       # Config for GCP
       :provider                         => 'Google',
-      :google_storage_access_id_key     => ENV['GCP_ACCESS_KEY'],
+      :google_storage_access_key_id     => ENV['GCP_ACCESS_KEY'],
       :google_storage_secret_access_key => ENV['GCP_KEY_SECRET']
     }
-    config.fog_directory = ENV['GCP_BUCKET']
+    conf.fog_directory = ENV['GCP_BUCKET']
   end
 end
